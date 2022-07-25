ca6418c25dd64ffa66156d1efd1c642d638cd6c4
Henock <henock.berhane@vizientinc.com>
2022-07-25 16:02:26 -0500

    replaced callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,10 @@ export const environment = {
   api: 'http://localhost:8081',
   disableAnimations: false,
   gaTrackingId: null,
-  // tslint:disable-next-line:max-line-length
-  // logoutRedirectUrl: 'https://sit-sso.alliancewebs.net/SsoCookieManager.axd?Action=Logout&ReturnURL=https://notification.test.vizientinc.com/admin/applications',
   icrIcuTimeout: 300000,
   issuer: `https://dev-61830595.okta.com/oauth2/default`,
   clientId: '0oa5wqh5cg53YB8dl5d7',
-  redirectUri: 'http://localhost:4200/login/callback',
+  redirectUri: 'http://coursesignupbucket.s3-website-us-east-1.amazonaws.com/login/callback',
   scopes: ['openid', 'email'],
   pkce: false,
   thisUri: 'http://localhost:4200',
